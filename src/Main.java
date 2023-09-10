c47724643334adbffa4a9aea785b05a97bce5db6
alibabaum <143457879+alibabaum@users.noreply.github.com>
2023-09-10 16:50:52 -0600

    Updated Kelvin Conversion

--- src/Main.java
+++ src/Main.java
@@ -11,8 +11,8 @@ public class Main {
         //This is for the input, has to be double so you can have decimals
         double kelNum=0;
         double celNum=0;
-        kelNum = (num + 273.15);
         celNum = (num - 32)*(5.0/9.0);
+        kelNum = (celNum + 273.15);
         //can't do 5/9 because you said it's a double, so you have to add the .0 so it thinks its a double
         System.out.println( "In Kelvin " + num + "is " + kelNum);
         System.out.println( "In Celsius " + num + "is " + celNum);
